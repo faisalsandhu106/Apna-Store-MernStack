1cfcff9ba50be1497cbd8328e166bde6fb7f88c0
Muhammad Faisal Nazir <faisalsandhu106@gmail.com>
2026-01-20 14:56:02 +0500

    Comment CART_TOTAL_ITEM_PRICE logic
    Commented out the CART_TOTAL_ITEM_PRICE action logic in CartReducer.

--- frontend/src/Components/Reducer/CartReducer.jsx
+++ frontend/src/Components/Reducer/CartReducer.jsx
@@ -73,22 +73,22 @@ const CartReducer = (state, action) => {
     };
 
     //* Total Item In CartSection
-    if (action.type === "CART_TOTAL_ITEM_PRICE") {
-        const { total_item, total_price } = state.cart.reduce((accum, curElem) => {
-            let { price, amount } = curElem
+    // if (action.type === "CART_TOTAL_ITEM_PRICE") {
+    //     const { total_item, total_price } = state.cart.reduce((accum, curElem) => {
+    //         let { price, amount } = curElem
 
-            accum.total_item += amount
-            accum.total_price += price * amount
+    //         accum.total_item += amount
+    //         accum.total_price += price * amount
 
-            return accum;
-        }, { total_item: 0, total_price: 0, })
+    //         return accum;
+    //     }, { total_item: 0, total_price: 0, })
 
-        return {
-            ...state,
-            total_item,
-            total_price,
-        }
-    };
+    //     return {
+    //         ...state,
+    //         total_item,
+    //         total_price,
+    //     }
+    // };
 
     return state
 
